3365e8e163f20f874962f57ec4d6bf3cdb30f74a
Patrick Moreau <patrick@drifterapps.com>
2026-08-05 23:28:10 -0400

    fix(prettier): update day

--- src/schedule/core/schedule-utils.ts
+++ src/schedule/core/schedule-utils.ts
@@ -1,6 +1,13 @@
 import type { ScheduleClass } from './schedule-class.ts';
 
-export type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
+export type DayOfWeek =
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thursday'
+  | 'Friday'
+  | 'Saturday'
+  | 'Sunday';
 
 export const DAYS_OF_WEEK: DayOfWeek[] = [
   'Monday',
